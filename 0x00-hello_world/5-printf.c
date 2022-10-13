b1c42af9863b20c2ea17a5af4df98a76f2600150
Minmiimii <maryammibrahimmm@gmail.com>
2022-10-13 16:12:18 -0700

    commit again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * maiin - Entry point
  *
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
 }
